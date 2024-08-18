cbfb29551fc15137a26152e6c11c894427939b6a
Jan Schaumann <jschauma@netmeister.org>
2024-08-18 14:47:24 -0400

    update for NetBSD 10.0

--- html/utm/start-apue.sh
+++ html/utm/start-apue.sh
@@ -3,16 +3,17 @@
 # Helper script to start a NetBSD VM using qemu.
 #
 # See https://stevens.netmeister.org/631/utm/ for more information.
-#
 
-# timeout(1) isn't necessarily present on macOS
-tout="$(which timeout 2>/dev/null)"
-if [ -z "${tout}" ]; then
-	function timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
+which timeout >/dev/null 2>&1
+if [ $? -gt 0 ]; then
+	# e.g., macOS; we assume Perl is present
+	timeout() { perl -e 'alarm shift; exec @ARGV' "$@"; }
 fi
 
 
 echo "=> Starting VM..."
+qcow=$(echo /Users/${USER}/Library/Containers/com.utmapp.UTM/Data/Documents/apue.utm/Data/*.qcow2)
+
 qemu-system-aarch64 \
 	-L /Applications/UTM.app/Contents/Resources/qemu  	\
 	-smp cpus=4,sockets=1,cores=4,threads=1 		\
@@ -21,19 +22,19 @@ qemu-system-aarch64 \
 	-cpu host 						\
 	-M virt,highmem=off 					\
 	-drive file=/Applications/UTM.app/Contents/Resources/qemu/edk2-aarch64-code.fd,if=pflash,format=raw,readonly=on         		\
-	-drive file=/Users/${USER}/Library/Containers/com.utmapp.UTM/Data/Documents/apue.utm/Images/data.qcow2,format=qcow2     				\
-	-nographic \
-	-serial tcp::4444,server,telnet,nowait \
+	-drive file=${qcow},format=qcow2			\
+	-nographic 						\
+	-serial tcp::4444,server,telnet,nowait 			\
 	-net user,hostfwd=tcp::2222-:22 -net nic >/dev/null 2>&1 &
 
 /bin/echo -n "==> Waiting for VM to boot..."
 n=1
 while [ 1 ]; do
-	timeout 2 "echo | nc localhost 2222 >/dev/null 2>&1" >/dev/null 2>&1
+	timeout 2 /bin/sh -c "echo | nc localhost 2222 >/dev/null 2>&1" >/dev/null 2>&1
 	if [ $? -eq 0 ]; then
-		# we still need one or two more seconds
+		# we still need a few more seconds
 		/bin/echo -n "."
-		sleep 2
+		sleep 3
 		echo;
 		echo "=> VM up and ready to log in."
 		break
